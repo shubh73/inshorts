d8e3244272781edba4aaf1c1657c297381f3e61a
shubh73 <shubhporwal73@gmail.com>
2021-07-20 14:16:10 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,147 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
+import { Button, LinkBox, Spacer, useColorMode, useDisclosure, Wrap, WrapItem, Text, Tag, Icon, TagLabel, Avatar } from "@chakra-ui/react";
+import { Stack, Flex, Box, Heading, LinkOverlay } from '@chakra-ui/layout';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton} from "@chakra-ui/react"
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from "@chakra-ui/react"
+import { IconButton } from '@chakra-ui/button';
+import { FaSun, FaMoon, FaInfo, FaUsers, FaNewspaper, FaRegNewspaper } from "react-icons/fa";
+import wordsToNumbers from "words-to-numbers";
 import './App.css';
 
-function App() {
+const alanKey = "4baadf286113f89f01994a830f6e95772e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+
+  // Modal
+  const { isOpen, onOpen, onClose} = useDisclosure();
+  const [modalPage, setModalPage] = useState([
+    { title: "Page 1", body: "page 1", pageNo: 1}, 
+    { title: "Page 2", body: "page 2", pageNo: 2 },
+    { title: "Page 3", body: "page 3", pageNo: 3 },
+  ]);
+
+  // Theme
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if(command === "newHeadlines"){
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        }
+        else if(command === "highlight"){
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        }
+        else if(command === "open"){
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if(parsedNumber > 20){
+            alanBtn().playText("Please try that again.")
+          }
+          else if(article){
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          }
+        }
+      }
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+       <Stack p={4}>
+            <Flex w="100%">
+              <LinkBox>
+              <LinkOverlay href=".">
+                <Button variant="ghost">
+                  <Heading 
+                    size="md" 
+                    fontWeight="bold" 
+                    color={isDark ? "teal.300" : "gray.900"}>
+                    <Icon as={FaRegNewspaper} mr={2} mb={1} ></Icon>
+                    Inshorts
+                  </Heading>
+                </Button>
+              </LinkOverlay>
+              </LinkBox>
+                <Spacer />
+                <Wrap spacing={3}>
+                  <WrapItem>
+                    <IconButton icon={<FaInfo />} onClick={onOpen}></IconButton>
+
+                    {/* Modal */}
+                    <Modal isOpen={isOpen} onClose={onClose} size="md" isCentered motionPreset="slideInRight">
+                      <ModalOverlay />
+                      <ModalContent>
+                        <ModalHeader>Instructions</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                          {/* Accordion */}
+                          <Accordion defaultIndex={[0]} allowToggle>
+                            <AccordionItem>
+                              <h2>
+                                <AccordionButton>
+                                  <Box flex="1" textAlign="left">
+                                    <Tag variant="ghost" p={0}>
+                                      <TagLabel>Small Talk</TagLabel> 
+                                        <Icon as={FaUsers } ml={2} mt={0.3} fontSize="lg"></Icon>
+                                    </Tag> 
+                                  </Box>
+                                  <AccordionIcon />
+                                </AccordionButton>
+                              </h2>
+                              <AccordionPanel pb={4} >
+                              Smart News includes small talk just like Siri and Alexa.
+                              You can click on the mic button to ask questions such as   
+                              <Tag p={1} ml={1}>How are you ?</Tag> <Tag p={1}>How old are you ?</Tag> <Tag p={1}>Where do you live ?</Tag> and a lot more.
+                              </AccordionPanel>
+                            </AccordionItem>
+
+                            <AccordionItem>
+                              <h2>
+                                <AccordionButton>
+                                  <Box flex="1" textAlign="left">
+                                    <Tag variant="ghost" p={0}>
+                                        <TagLabel>News</TagLabel> 
+                                          <Icon as={FaNewspaper} ml={2} mt={0.8} fontSize="md"></Icon>
+                                    </Tag> 
+                                  </Box>
+                                  <AccordionIcon />
+                                </AccordionButton>
+                              </h2>
+                              <AccordionPanel pb={4} >
+                                You can start off by clicking on the mic and asking <Tag p={1}>What is this project ?</Tag> or <Tag p={1} mr={1}>What is this ? </Tag> 
+                                to get a gist of what the project is about, and then move on to asking multiple questions 
+                                related to news on any topic based on the commands shown. <br/>
+                                You can also ask Alan to open an article by saying <Tag p={1}>Open article 4</Tag> as well as go back using the command <Tag p={1} mt={0.5} >Go back.</Tag>
+                              </AccordionPanel>
+                            </AccordionItem>
+                          </Accordion>
+                        </ModalBody>
+
+                        <ModalFooter>
+                          <Button colorScheme="blue" mr={3} onClick={onClose}>
+                            Close
+                          </Button>
+                        </ModalFooter>
+                      </ModalContent>
+                    </Modal>
+                  </WrapItem>
+                  <WrapItem>
+                    <IconButton icon={isDark ? <FaSun /> : <FaMoon />} onClick={toggleColorMode}></IconButton>
+                  </WrapItem>
+                </Wrap>
+            </Flex>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+        </Stack>
     </div>
   );
 }
---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,229 @@
+import React from 'react';
+import NewsCard from "./NewsCard/NewsCard";
+import { useColorMode } from '@chakra-ui/color-mode';
+import { Badge, Box, Image, Stack, Text, Wrap, Tag, Tooltip, WrapItem } from '@chakra-ui/react';
+import { FaMicrophoneAlt } from 'react-icons/fa';
+import Img1 from "../../assets/1.png";
+import Img2 from "../../assets/4.png";
+import Img3 from "../../assets/3.png";
+import Img7 from "../../assets/7.png";
+
+const NewsCards = ({ articles, activeArticle }) => {
+    const { colorMode } = useColorMode();
+    const bgColor = {light: "gray.200", dark: "gray.700"};
+
+    if(!articles.length){
+        return(
+            <Wrap 
+                flexWrap="wrap" 
+                justify="center"
+                p={5}
+                spacing={7}>
+                {/* Box 1 */}
+                <WrapItem>
+                    <Box 
+                        w="300px" 
+                        rounded="20px" 
+                        overflow="hidden" 
+                        boxShadow="lg" 
+                        bg={bgColor[colorMode]}>
+                        <Image src={Img1} alt="Latest News"/>
+                        <Box p={5}>
+                            <Stack align="baseline" alignItems="center" h={28}>
+                                <Badge 
+                                    variant="outline" 
+                                    colorScheme="teal" 
+                                    rounded="full" 
+                                    px={2}
+                                    fontSize="md"
+                                    fontWeight="semibold">
+                                    News by Countries
+                                </Badge>
+                                <Wrap spacing={1} justify="center" pt={3}>
+                                    <Tag>India</Tag>
+                                    <Tag>Australia</Tag>
+                                    <Tag>France</Tag>
+                                    <Tag>China</Tag>
+                                    <Tag>America</Tag>
+                                    <Tag>Germany</Tag>
+                                    <Tag>Japan</Tag>
+                                    <Tag>Saudi Arabia</Tag>
+                                </Wrap>
+                            </Stack>
+                        </Box>
+                        <Box p={5}>
+                            <Stack isInline justify="center">
+                                <Tooltip label="Try saying" fontSize="xs" >
+                                    <Tag variant="ghost" size="md" p={0}><FaMicrophoneAlt cursor="pointer" /></Tag>
+                                </Tooltip>
+                                <Text 
+                                    fontSize="sm" 
+                                    fontWeight="semibold" 
+                                    textTransform="none"
+                                    letterSpacing="wide">
+                                    Top headlines from <Text as="u" >India</Text>
+                                </Text>
+                            </Stack>
+                        </Box>
+                    </Box>
+                </WrapItem>
+                {/* Box 2 */}
+                <WrapItem>
+
+                    <Box 
+                        w="300px" 
+                        rounded="20px" 
+                        overflow="hidden" 
+                        boxShadow="lg" 
+                        bg={bgColor[colorMode]}>
+                        <Image src={Img7} alt="News by Categories"/>
+                        <Box p={5}>
+                            <Stack align="baseline" alignItems="center" h={28}>
+                                <Badge 
+                                    variant="outline" 
+                                    colorScheme="teal" 
+                                    rounded="full" 
+                                    px={2}
+                                    fontSize="md"
+                                    fontWeight="semibold">
+                                    News by Categories
+                                </Badge>
+                                <Wrap spacing={1} justify="center" pt={3}>
+                                    <Tag>Business</Tag>
+                                    <Tag>Entertainment</Tag>
+                                    <Tag>General</Tag>
+                                    <Tag>Health</Tag>
+                                    <Tag>Science</Tag>
+                                    <Tag>Sports</Tag>
+                                    <Tag>Technology</Tag>
+                                </Wrap>
+                            </Stack>
+                        </Box>
+                        <Box p={5}>
+                            <Stack isInline justify="center" >
+                                <Tooltip label="Try saying" fontSize="xs">
+                                    <Tag variant="ghost" size="md" p={0} ><FaMicrophoneAlt cursor="pointer" /></Tag>
+                                </Tooltip>
+                                <Text 
+                                    fontSize="sm" 
+                                    fontWeight="semibold" 
+                                    textTransform="none"
+                                    letterSpacing="wide">
+                                    Give me the latest <Text as="u" >health</Text>  news
+                                </Text>
+                            </Stack>
+                        </Box>
+                    </Box>
+                </WrapItem>
+                {/* Box 3 */}
+                <WrapItem>
+
+                    <Box 
+                        w="300px" 
+                        rounded="20px" 
+                        overflow="hidden" 
+                        boxShadow="lg" 
+                        bg={bgColor[colorMode]}>
+                        <Image src={Img3} alt="News by Terms"/>
+                        <Box p={5}>
+                            <Stack align="baseline" alignItems="center" h={28}>
+                                <Badge 
+                                    variant="outline" 
+                                    colorScheme="teal" 
+                                    rounded="full" 
+                                    px={2}
+                                    fontSize="md"
+                                    fontWeight="semibold">
+                                    News by Terms
+                                </Badge>
+                                <Wrap spacing={1} justify="center" pt={3} >
+                                    <Tag>Bitcoin</Tag>
+                                    <Tag>Smartphones</Tag>
+                                    <Tag>Gadgets</Tag>
+                                    <Tag>Stock Market</Tag>
+                                    <Tag>Coronavirus</Tag>
+                                    <Tag>Donald Trump</Tag>
+                                </Wrap>
+                            </Stack>
+                        </Box>
+                        <Box p={5}>
+                            <Stack isInline justify="center" >
+                                <Tooltip label="Try saying" fontSize="xs" >
+                                    <Tag variant="ghost" size="md" p={0}><FaMicrophoneAlt cursor="pointer" /></Tag>
+                                </Tooltip>
+                                <Text 
+                                    fontSize="sm" 
+                                    fontWeight="semibold" 
+                                    textTransform="none"
+                                    letterSpacing="wide">
+                                    What's up with <Text as="u">stock market</Text>
+                                </Text>
+                            </Stack>
+                        </Box>
+                    </Box>
+                </WrapItem>
+                {/* Box 4 */}
+                <WrapItem>
+
+                    <Box 
+                        w="300px" 
+                        rounded="20px" 
+                        overflow="hidden" 
+                        boxShadow="lg" 
+                        bg={bgColor[colorMode]}>
+                        <Image src={Img2} alt="News by Sources"/>
+                        <Box p={5}>
+                            <Stack align="baseline" alignItems="center" h={28}>
+                                <Badge 
+                                    variant="outline" 
+                                    colorScheme="teal" 
+                                    rounded="full" 
+                                    px={2}
+                                    fontSize="md"
+                                    fontWeight="semibold">
+                                    News by Sources
+                                </Badge>
+                                <Wrap spacing={1} justify="center" pt={3} >
+                                    <Tag>CNN</Tag>
+                                    <Tag>Wired</Tag>
+                                    <Tag>BBC News</Tag>
+                                    <Tag>ABC</Tag>
+                                    <Tag>Fox News</Tag>
+                                    <Tag>ESPN</Tag>
+                                    <Tag>Buzzfeed</Tag>
+                                    <Tag>Bloomberg</Tag>
+                                </Wrap>
+                            </Stack>
+                        </Box>
+                        <Box p={5}>
+                            <Stack isInline justify="center" >
+                                <Tooltip label="Try saying" fontSize="xs" >
+                                    <Tag variant="ghost" size="md" p={0}><FaMicrophoneAlt cursor="pointer" /></Tag>
+                                </Tooltip>
+                                <Text 
+                                    fontSize="sm" 
+                                    fontWeight="semibold" 
+                                    textTransform="none"
+                                    letterSpacing="wide">
+                                    Give me the news from <Text as="u" >CNN</Text>
+                                </Text>
+                            </Stack>
+                        </Box>
+                    </Box>
+                </WrapItem>
+            </Wrap>
+        );
+    }
+
+    return (
+        <Wrap w="100%" spacing={4} justify="center" p={4}>
+            {articles.map((article, i) => (
+                <Box display="flex" flexWrap="wrap">
+                    <NewsCard article={article} activeArticle={activeArticle} i={i} />
+                </Box>
+            ))}
+        </Wrap>
+    );
+};
+
+export default NewsCards;
